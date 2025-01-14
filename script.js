c24ad7bdacc74b7b3f92fccc0c8d02a157ac6b67
MichaelS96 <m.schwittay96@web.de>
2025-01-14 11:24:23 +0100

    test commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let currentIndex = 1;
 const limit = 22;
-let pokedex, loadButton, loadingImg, modal, modalContent;
+let pokedex, loadButton, loadingImg, modal, modalContent, searchInput;
+let allPokemons = [];
 
 function renderPokedex() {
     pokedex = document.getElementById('pokedex');
@@ -8,6 +9,7 @@ function renderPokedex() {
     loadingImg = document.getElementById('loading');
     modal = document.getElementById('pokemonModal');
     modalContent = document.getElementById('pokemonModalContent');
+    searchInput = document.getElementById('searchInput');
 
     loadButton.onclick = getPokemons;
     getPokemons();
@@ -21,10 +23,11 @@ async function getPokemons() {
         const end = currentIndex + limit - 1;
 
         for (let i = currentIndex; i <= end; i++) {
-            if (i > 151) break; // zählt den Index nur bis 151 
+            if (i > 151) break;
 
             const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
             const pokemonData = await response.json();
+            allPokemons.push(pokemonData);
             createPokemonCard(pokemonData);
 
             await delay(150);
@@ -34,6 +37,32 @@ async function getPokemons() {
     } finally {
         loadingImg.style.display = 'none';
         loadButton.disabled = false;
+    }
+}
+
+function filterPokemons() {
+
+    const searchQuery = searchInput.value.toLowerCase();
+
+    if (searchQuery.length < 3) {
+        pokedex.innerHTML = ''; 
+        for (let i = 0; i < allPokemons.length; i++) {
+            createPokemonCard(allPokemons[i]);
+        }
+        return;
+    }
+
+    const filteredPokemons = [];
+
+    for (let i = 0; i < allPokemons.length; i++) {
+        if (allPokemons[i].name.toLowerCase().includes(searchQuery)) {
+            filteredPokemons.push(allPokemons[i]);
+        }
+    }
+
+    pokedex.innerHTML = '';
+    for (let i = 0; i < filteredPokemons.length; i++) {
+        createPokemonCard(filteredPokemons[i]);
     }
 }
 
@@ -62,15 +91,14 @@ function closeModal() {
     modal.style.display = 'none';
 }
 
-
 async function getMoves(pokemon) {
-    const moves = pokemon.moves.slice(0, 4); 
+    const moves = pokemon.moves.slice(0, 4);
 
     const moveData = await Promise.all(
         moves.map(async (move) => {
             const response = await fetch(move.move.url);
             const moveDetails = await response.json();
-            const power = moveDetails.power || '0'; 
+            const power = moveDetails.power || '0';
             return {
                 name: move.move.name,
                 power: power
@@ -86,4 +114,26 @@ function switchTab(tabId) {
     contents.forEach(content => content.classList.add('hidden'));
 
     document.getElementById(tabId).classList.remove('hidden');
+}
+
+function showNextPokemon() {
+    currentPokemonIndex = currentPokemonIndex + 1;
+
+    if (currentPokemonIndex < 0) {
+        currentPokemonIndex = allPokemons.length + 1;
+    }
+
+    const nextPokemon = allPokemons[currentPokemonIndex];
+    openCard(nextPokemon);
+}
+
+function showPreviousPokemon() {
+    currentPokemonIndex = currentPokemonIndex - 1;
+
+    if (currentPokemonIndex < 0) {
+        currentPokemonIndex = allPokemons.length - 1;
+    }
+
+    const previousPokemon = allPokemons[currentPokemonIndex];
+    openCard(previousPokemon);
 }
